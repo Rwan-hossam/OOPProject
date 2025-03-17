cf5929f1aa393071cdcced24f37ea4540cc343d6
Rwan Hosam <rwanhossam0@gmail.com>
2025-03-17 21:29:55 +0200

    main

--- bin/OOPProject.dart
+++ bin/OOPProject.dart
@@ -1,1 +1,18 @@
-void main(List<String> arguments) {}
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Welcome to the Bank Management');
+  print('Choose an Operation');
+  print('''1. Create Account
+2. Deposit Money
+3. Withdraw Money
+4. Check Balance
+5. View Transactions
+6. Exit''');
+  int operationNum = int.parse(stdin.readLineSync()!);
+  switch (operationNum) {
+    case 1:
+      break;
+    default:
+  }
+}
