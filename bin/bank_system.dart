e8cba9accb1292319e351f53d998dda7689b6665
Rwan Hosam <rwanhossam0@gmail.com>
2025-03-17 20:59:44 +0200

    main

--- bin/bank_system.dart
+++
@@ -1,5 +0,0 @@
-import 'package:bank_system/bank_system.dart' as bank_system;
-
-void main(List<String> arguments) {
-  print('Hello world: ${bank_system.calculate()}!');
-}
---
+++ bin/main.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:bank_system/bank_system.dart' as bank_system;
+import 'bankAccount.dart';
+
+void main(List<String> arguments) {
+  print('Welcome to the Bank Management');
+  print('Choose an Operation');
+  print('''1. Create Account
+2. Deposit Money
+3. Withdraw Money
+4. Check Balance
+5. View Transactions
+6. Exit''');
+  int operationNum = int.parse(stdin.readLineSync()!);
+  switch (operationNum) {
+    case 1:
+      
+      break;
+    default:
+  }
+}
