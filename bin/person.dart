3a4f63f8dc76b1f74325ff08fa0a18a6ec53f38a
Habiba-Abo-Hashish <habibaabohashish84@gmail.com>
2025-03-18 22:55:36 +0200

    Person class & enum

--- bin/person.dart
+++ bin/person.dart
@@ -2,14 +2,11 @@ abstract class Person {
   String Name;
   int ID;
   int Age;
-  CustomerType customerType;
+
   String address;
   GenderType genderType;
-  Person(this.Name, this.customerType, this.ID, this.Age, this.address,
-      this.genderType);
+  Person(this.Name, this.ID, this.Age, this.address, this.genderType);
   void role();
 }
-
-enum CustomerType { VIP, Rgular, Premium }
 
 enum GenderType { Male, Female }
